6672ad9a8b500e36af133cc088a9262ac0978174
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-07-07 14:54:24 +0530

    Time: 0 ms (100%), Space: 8.9 MB (89.09%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -9,7 +9,7 @@ public:
         while(n > 0){
             int d = n % 10;
 
-            if(d != 0){
+            if(d > 0){
                 rem = d * place + rem; // for creating a number;
                 place *= 10; // for placing a digit in their apporciate place;
                 sum += d;  // sum of digits;
