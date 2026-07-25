e4054e1b5f7b6ac7a834b39921e0a121a8840396
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-07-25 16:28:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (74.03%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -6,21 +6,19 @@ public:
         int digit;
         while(n > 0){
          digit = n % 10;
-        
         n = n / 10;
 
-        
         if (digit > largest) {
-    secondLargest = largest;
-    largest = digit;
-    }
-    else if (digit > secondLargest) {
-        secondLargest = digit;
+        secondLargest = largest;
+        largest = digit;
+                }
+         else if (digit > secondLargest) {
+            secondLargest = digit;
 
 
-        largest = max(largest, digit);
+            largest = max(largest, digit);
         }
-        }
+     }
 
 
         return largest * secondLargest;
