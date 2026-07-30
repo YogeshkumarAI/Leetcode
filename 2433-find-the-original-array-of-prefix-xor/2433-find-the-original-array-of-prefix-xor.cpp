55832c2329ede574a67a392908ea2d334e0cb3a9
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-07-30 09:15:11 +0530

    Time: 6 ms (12.18%), Space: 83.8 MB (6.85%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -5,8 +5,8 @@ public:
 
             arr.push_back(pref[0]);
         for(int i = 1; i < pref.size(); i++){
-            int x = pref[i] ^ pref[i-1];
-            arr.push_back(x); 
+            // int x = pref[i] ^ pref[i-1];
+            arr.push_back(pref[i] ^ pref[i - 1]); 
         }
         return arr;
     }
