5344d9ac5be45e5d5818fbf98d40f07e82b411b0
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-07-31 12:35:06 +0530

    Time: 0 ms (100%), Space: 7.9 MB (29.56%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+    
+    string s = bitset<32>(n).to_string();
+    s = s.substr(s.find('1'));
+
+    while (s.length() < 4)
+        s = "0" + s;
+
+    int left = 0;
+    int right = s.size()-1;
+
+    while(left <= right){
+        if(left == right){
+            left++;
+            right--;
+        }
+return false;
+    }
+        return true;
+    }
+};
