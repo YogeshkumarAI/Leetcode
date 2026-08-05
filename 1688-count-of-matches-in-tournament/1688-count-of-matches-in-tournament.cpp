143793f49693d08856cb5a1e531735b8b1c04945
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-08-05 14:49:55 +0530

    Time: 0 ms (100%), Space: 8 MB (37.19%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int eventeams = n / 2;
-        int oddteams = (n - 1) / 2;
-
-
-        int EvenTmMtch = eventeams;
-        int OddTmMtch = oddteams + 1;
-
-        int even = eventeams + EvenTmMtch - 1;
-        int odd = oddteams + OddTmMtch - 1;
-
-        return n % 2 == 0 ? even : odd;
+        return n - 1;
     }
 };
