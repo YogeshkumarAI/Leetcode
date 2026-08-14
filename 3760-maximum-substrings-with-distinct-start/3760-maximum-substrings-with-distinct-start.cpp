f05e6752ddd65b033a5f3aa668510c9c6327f916
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-08-14 19:05:39 +0530

    Time: 43 ms (16.14%), Space: 25.4 MB (8.07%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        unordered_set<char> st;
+        set<char> st;
 
         for(int i = 0; i < s.size(); i++){
             st.insert(s[i]);
