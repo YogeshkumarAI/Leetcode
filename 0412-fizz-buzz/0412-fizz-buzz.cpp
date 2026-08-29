7d2686fea42f1af266ec3c2f65cb896d88eb35da
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-08-29 22:07:03 +0530

    Time: 0 ms (100%), Space: 12 MB (17.72%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         
-        vector<string> ans;
+        vector<string>ans;
+
+        int count3 = 0;
+        int count5 = 0;
+
         for(int i = 1; i <= n; i++){
-            if(i % 3 == 0 && i % 5 == 0){
+            count3++;
+            count5++;
+
+            if(count3 % 3 == 0 && count5 % 5 == 0){
                 ans.push_back("FizzBuzz");
-            }
-            else if(i % 3 == 0){
+                count3 = 0;
+                count5 = 0;
+             }
+            else if(count3 % 3 == 0){
                 ans.push_back("Fizz");
+                count3 = 0;
             }
-            else if(i % 5 == 0){
+            else if(count5 % 5 == 0){
                 ans.push_back("Buzz");
+                count5 = 0;
             }
             else{
                 ans.push_back(to_string(i));
