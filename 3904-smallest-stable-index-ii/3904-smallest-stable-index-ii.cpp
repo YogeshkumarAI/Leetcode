af7931c9594ba2a3f8a42b06c63bd8351f1cc430
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-09-04 16:39:17 +0530

    Time: 15 ms (40.11%), Space: 202.5 MB (96.85%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -3,23 +3,22 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         
         int n = nums.size();
+        vector<int>mineleindex(n);
         int minele = INT_MAX; 
 
-        vector<int>MinimumElementIndex(n);
         for(int i = n - 1; i >= 0; i--){
             minele = min(minele, nums[i]);
-            MinimumElementIndex[i] = minele;
+            mineleindex[i] = minele;
         }
 
 
         int maxele = INT_MIN;
         for(int i = 0; i < n; i++){
             maxele = max(maxele, nums[i]); // maxelement;
-            // minele = MinimumElementIndex[i];
+            // minele = mineleindex[i];
 
-            if(maxele - MinimumElementIndex[i] <= k){
-                return i;
-            }
+            if(maxele - mineleindex[i] <= k)
+                return i;         
         }
         return -1;
     }
