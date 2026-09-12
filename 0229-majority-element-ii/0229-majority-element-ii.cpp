b5fb3a59d11401bb01b688db2a2f6c6c62c1540e
CodeWithYogesh7 <yogeshsingh4088@gmail.com>
2026-09-12 16:28:40 +0530

    Time: 10 ms (21.63%), Space: 27.3 MB (5.3%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int MOD = 10000000007;
+         map<int, int> mp;
+
+        for(auto x : nums){
+            mp[x]++;
+        }
+
+    int x = (n / 3) % MOD;
+        vector<int>ans;
+        for(auto it : mp){
+            if(it.second > x){
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
+    }
+};
